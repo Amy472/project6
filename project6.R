c24431d06f6f9bb6a62c24d47555143b22f9332b
Amy472 <hang.vo@lc.cuny.edu>
2024-12-10 17:54:40 -0500

    Update project6.R

--- project6.R
+++ project6.R
@@ -1,4 +1,4 @@
 
-plot(NA_Sales~JP_Sales, data = vgsales, xlab = "JP_Sales (copies of a video game sold)", ylab = "NA_Sales (copies of a video game sold)", main = "Relationship Between Sales in Japan and North America")
+plot(NA_Sales~JP_Sales, data = vgsales, xlab = "JP_Sales (million copies of a video game sold)", ylab = "NA_Sales (million copies of a video game sold)", main = "Relationship Between Sales in Japan and North America")
 cor(vgsales$NA_Sales, vgsales$JP_Sales)
 # the correlation between NA_Sales and JP_Sales is 0.4497874
